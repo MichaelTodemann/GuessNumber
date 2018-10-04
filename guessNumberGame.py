7ed01c6d78918e821db574b0531c26d87d383a41
MichaelTodemann <michael.todemann@gmail.com>
2018-10-04 19:09:16 -0400

    Added a input control so it only takes integer

--- guessNumberGame.py
+++ guessNumberGame.py
@@ -1,22 +1,25 @@
 import sys, random
 
-Ran_num = random.randint(1,20)
-count = 1
+def control_int():
+    while True:
+        try:
+            guess = int(input("Take a guess > "))
+            return guess
+        except:
+            print("THAT IS NOT A NUMBER! Try again.")
+            continue
 
-print("I am thinking of a number between 1 and 20.")
 
 def engine(Ran_num, count):
-
     while True:
-        guess = int(input("Take a guess  > "))
-
+        guess = control_int()
         if guess > Ran_num:
-            print("Wrong! Your guess is higher. Try a lower number.")
+            print("Wrong! Your number is higher. Try a lower number.")
             count += 1
             continue
 
         elif guess < Ran_num:
-            print("Wrong! Your guess is lower. Try a higher number.")
+            print("Wrong! Your number is lower. Try a higher number.")
             count += 1
             continue
 
@@ -24,5 +27,10 @@ def engine(Ran_num, count):
             print(f"Correct! The answer was {Ran_num}")
             print(f"You guessed it in {count} tries.")
             sys.exit()
+
+
+print("I am thinking of a number between 1 and 20.")
+Ran_num = random.randint(1,20)
+count = 1
 
 engine(Ran_num, count)
